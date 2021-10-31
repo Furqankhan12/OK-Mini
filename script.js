f8fee78eee42ab015e0cbe835435c49e0563cbd5
Ghulam Hussain Joyo <ghulamhussain.kcsf18@iba-suk.edu.pk>
2021-10-31 11:33:05 +0500

    round 2 to fixed

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ function myFunction()
   let value = parseInt(document.getElementById("value").value);
   if (value != NaN) {
 
-    let step1 =  (value * (0.6 / 100)).toFixed(3)
-    let step2 = (value * (1.2 / 100)).toFixed(3)
-    let step3 = (value * (2.6 / 100)).toFixed(3);
-    let step4 = (value * (5.6 / 100)).toFixed(3);
-    let step5 =  (value * (12 / 100)).toFixed(3);
-    let step6 = (value * (25 / 100)).toFixed(3);
-    let step7 = (value * (53 / 100)).toFixed(3);
+    let step1 =  (value * (0.6 / 100)).toFixed(2)
+    let step2 = (value * (1.2 / 100)).toFixed(2)
+    let step3 = (value * (2.6 / 100)).toFixed(2);
+    let step4 = (value * (5.6 / 100)).toFixed(2);
+    let step5 =  (value * (12 / 100)).toFixed(2);
+    let step6 = (value * (25 / 100)).toFixed(2);
+    let step7 = (value * (53 / 100)).toFixed(2);
 
-    let winAmount1 = (step1*1.95).toFixed(3);
-    let winAmount2 = (step2 * 1.95).toFixed(3)
-    let winAmount3 = (step3 * 1.95).toFixed(3)
-    let winAmount4 = (step4 * 1.95).toFixed(3)
-    let winAmount5 = (step5 * 1.95).toFixed(3)
-    let winAmount6 = (step6 * 1.95).toFixed(3)
-    let winAmount7 = (step7 * 1.95).toFixed(3)
+    let winAmount1 = (step1*1.95).toFixed(2);
+    let winAmount2 = (step2 * 1.95).toFixed(2)
+    let winAmount3 = (step3 * 1.95).toFixed(2)
+    let winAmount4 = (step4 * 1.95).toFixed(2)
+    let winAmount5 = (step5 * 1.95).toFixed(2)
+    let winAmount6 = (step6 * 1.95).toFixed(2)
+    let winAmount7 = (step7 * 1.95).toFixed(2)
 
     
 
@@ -40,17 +40,17 @@ function myFunction()
     document.getElementById("winAmount6").innerText = winAmount6;
     document.getElementById("winAmount7").innerText = winAmount7;
 
-    document.getElementById("netProfit1").innerText = (winAmount1 - step1).toFixed(3);
-    document.getElementById("netProfit2").innerText = (parseFloat(winAmount2)-(parseFloat(step1)+parseFloat(step2))).toFixed(3);
-    document.getElementById("netProfit3").innerText = (parseFloat(winAmount3)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3))).toFixed(3);
+    document.getElementById("netProfit1").innerText = (winAmount1 - step1).toFixed(2);
+    document.getElementById("netProfit2").innerText = (parseFloat(winAmount2)-(parseFloat(step1)+parseFloat(step2))).toFixed(2);
+    document.getElementById("netProfit3").innerText = (parseFloat(winAmount3)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3))).toFixed(2);
 
-    document.getElementById("netProfit4").innerText = (parseFloat(winAmount4)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4))).toFixed(3);
+    document.getElementById("netProfit4").innerText = (parseFloat(winAmount4)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4))).toFixed(2);
 
-    document.getElementById("netProfit5").innerText = (parseFloat(winAmount5)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5))).toFixed(3);
+    document.getElementById("netProfit5").innerText = (parseFloat(winAmount5)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5))).toFixed(2);
 
-    document.getElementById("netProfit6").innerText = (parseFloat(winAmount6)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5)+parseFloat(step6))).toFixed(3);
+    document.getElementById("netProfit6").innerText = (parseFloat(winAmount6)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5)+parseFloat(step6))).toFixed(2);
     
-    document.getElementById("netProfit7").innerText = (parseFloat(winAmount7)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5)+parseFloat(step6)+parseFloat(step7))).toFixed(3);
+    document.getElementById("netProfit7").innerText = (parseFloat(winAmount7)-(parseFloat(step1)+parseFloat(step2)+parseFloat(step3)+parseFloat(step4)+parseFloat(step5)+parseFloat(step6)+parseFloat(step7))).toFixed(2);
 
 
 
